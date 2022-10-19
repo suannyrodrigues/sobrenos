33dad1761f45b6fbf0e220f410e1a876c60e3a81
Suanny Rodrigues <suannyrsantos@gmail.com>
2022-10-19 11:40:22 -0300

    Add files via upload

---
+++ script/array.js
@@ -0,0 +1,9 @@
+
+const pessoas = [
+    {nome: "Julio", idade: 22},
+    {nome: "Laura", idade: 35},
+    {nome: "Manu", idade: 15},
+    {nome: "Jefferson", idade: 17}
+   ];
+   const podemBeber = pessoas.filter( pes => pes.idade >= 18? true :
+   false);
